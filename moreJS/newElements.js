3520f53998d57724f4b52f4ec82ac1b23833e99b
Alejandra Jurado <ajurado2204@gmail.com>
2015-11-21 13:01:59 -0500

    pBtn added

--- moreJS/newElements.js
+++ moreJS/newElements.js
@@ -1,3 +1,9 @@
-$('p').click(function(){
-	
-})
+$(document).ready(initializer());
+
+function initializer(){
+	$('#pBtn').click(function(){
+		alert("hello");
+		$(".starter-template").append("<button>hello world</button>");
+	});
+
+}
